4975ee4a6564fda59e5ffd2a5e825909369b97ae
juhoon212 <ws5501@naver.com>
2023-05-02 11:06:46 +0900

    문제2

--- src/Programmers/Num2.java
+++ src/Programmers/Num2.java
@@ -12,8 +12,9 @@ public class Num2 {
 
         for (int i = 0; i < photo.length; i++) {
             for (int j = 0; j < photo[i].length; j++) {
+                // photo의 이차원 배열에 값이 있을때만
                 if(Arrays.asList(name).indexOf(photo[i][j]) != -1) {
-                    answer[i] += yearning[Arrays.asList(name).indexOf(photo[i][j])];
+                    answer[i] += yearning[Arrays.asList(name).indexOf(photo[i][j])]; // 규칙 찾기
                 }
             }
         }
