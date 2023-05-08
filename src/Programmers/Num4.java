1e768daeddfbb10ec6d5d60212aad30256f0819c
juhoon212 <ws5501@naver.com>
2023-05-08 12:51:09 +0900

    문제4

--- src/Programmers/Num4.java
+++ src/Programmers/Num4.java
@@ -1,2 +1,35 @@
-package Programmers;public class Num4 {
+package Programmers;
+
+public class Num4 {
+
+    // 다시 풀기
+    public static void main(String[] args) {
+        Num4 n = new Num4();
+        int[] solution = n.solution(new String[]{"..........", ".....#....", "......##..", "...##.....", "....#....."});
+        for (int i : solution) {
+            System.out.println(i);
+        }
+
+
+    }
+
+
+    public int[] solution(String[] wallpaper) {
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        for(int i=0; i< wallpaper.length;i++ ){
+            for(int j=0; j<wallpaper[i].length();j++){
+                if(wallpaper[i].charAt(j)=='#'){
+                    minX = Math.min(minX,i); // 1
+                    minY = Math.min(minY,j); // 5
+                    maxX = Math.max(maxX,i); // 1
+                    maxY = Math.max(maxY,j); // 5
+                    // 계속 반복해서 비교
+                }
+            }
+        }
+        return new int[]{minX,minY,maxX+1,maxY+1};
+    }
 }
---
+++ src/Programmers/Num6.java
@@ -0,0 +1,49 @@
+package Programmers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Num6 {
+
+    // 다시 풀기
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+
+        Set<String> set = new HashSet<>(Arrays.asList(keymap));
+        set.remove("");
+
+        for (int i = 0; i < targets.length; i++) { // targets
+
+            for (int l = 0; l < targets[i].length(); l++) { // char
+                int k = 101;
+
+                for (String j : set) { // set
+                    int index = j.indexOf(targets[i].charAt(l));
+
+                    if (index == -1) continue;
+                    else if (index < k) k = index; // k가 인덱스보다 크면 다음 배열에서 찾도록 필터역할
+                }
+
+
+                if (k == 101) {
+                    answer[i] = -1;
+                    break;
+                } else answer[i] += (k + 1); // 2 ,
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Num6 num = new Num6();
+        int[] solution = num.solution(new String[]{"ABACD", "BCEFD"}, new String[]{"ABCD", "AABB"});
+        for (int i : solution) {
+            System.out.println(i);
+        }
+    }
+
+
+
+}
