0882033d54a0d82c3311392f9585080b79566f54
juhoon212 <ws5501@naver.com>
2023-05-09 11:16:27 +0900

    문제7

---
+++ src/Programmers/Num7.java
@@ -0,0 +1,33 @@
+package Programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Num7 {
+
+    // 조금만 더 간단하게 생각해보기
+
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+
+        int cards1Num = 0;
+        int cards2Num = 0;
+
+        String answer = "";
+
+        for (int i = 0; i < goal.length; i++) {
+
+            String cursor = goal[i];
+
+            if(cards1Num < cards1.length && cursor.equals(cards1[cards1Num])) {
+                cards1Num++;
+            } else if (cards2Num < cards2.length && cursor.equals(cards2[cards2Num])) {
+                cards2Num++;
+            } else return "No";
+
+
+        }
+
+        return "Yes";
+    }
+}
