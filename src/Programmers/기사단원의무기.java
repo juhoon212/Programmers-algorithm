e28f41b7d2193657a0deeed8272f062c4bf1a20c
juhoon212 <ws5501@naver.com>
2023-05-25 11:23:52 +0900

    기사단원의 무기

--- src/Programmers/기사단원의무기.java
+++ src/Programmers/기사단원의무기.java
@@ -7,30 +7,23 @@ public class 기사단원의무기 {
 
     // 시간복잡도 추가로 제곱근을 이용해서 풀기!!
 
-    public static int solution(int number, int limit, int power) {
+    public int solution(int number, int limit, int power) {
 
-        int arr[] = new int[number + 1];
-        int answer = 0;
-
+        int numArray[] = new int[number + 1];
+        int total = 0;
         for (int i = 1; i <= number; i++) {
-            int num = count(i);
+            int divisor = countOfDivisor(i);
+            numArray[i] = divisor > limit ? power : divisor;
 
-            arr[i] = num > limit ? power : num;
-
-            answer += arr[i];
+            total += numArray[i];
         }
-
-        for (int i : arr) {
-            System.out.println(i);
-        }
-        return answer;
+        return total;
 
     }
 
-
-
-    private static int count(int num) {
+    private int countOfDivisor(int num) { // number까지의 약수의 갯수를 구하는 메소드
         int count = 0;
+
         for (int i = 1; i * i <= num; i++) {
             if(i * i == num) {
                 count++;
@@ -38,14 +31,8 @@ public class 기사단원의무기 {
                 count += 2;
             }
         }
-
         return count;
-    }
 
-    public static void main(String[] args) {
-        int solution = 기사단원의무기.solution(10, 3, 2);
-        System.out.println(solution);
     }
-
 
 }
